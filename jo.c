d36b0f4affa59087182f98cb6b5a9a5ec3262ce1
bouchrakd <bouchra.zamoum05@gmail.com>
2024-05-22 09:51:31 +0000

    Modification

--- jo.c
+++ jo.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
 #include <float.h>
 
+typedef struct{
+    int jour;
+    int mois;
+    int annee;
+} Date;
+
+typedef struct{
+    int minute;
+    int seconde;
+    int ms;
+} Temps;
+
 typedef struct {
-    char date[11];  // Format AAAA-MM-JJ
-    int epreuve;  // Type d'épreuve (100m, 400m, etc.)
-    float temps;  // Temps réalisé
+    Date date;  // Format AAAA-MM-JJ
+    char epreuve[100];  // Type d'épreuve (100m, 400m, etc.)
+    Temps temps;  // Temps réalisé
     int position_relais;  // Position dans le relais, -1 si non applicable
 } Performance;
 
@@ -23,10 +34,20 @@ typedef struct {
     float meilleurTemps;
     float pireTemps;
     float tempsMoyen;
-} StatistiquesEpreuve;  
+} StatistiquesEpreuve;
 
-void enregistrerAthlete(const char *nom_fichier, Performance performance) {
-    FILE *fichier = fopen(nom_fichier, "a"); 
+void typeEpreuves(){
+    printf("1. 100m\n");
+    printf("2. 400m\n");
+    printf("3. 5000m\n");
+    printf("4. marathon\n");
+    printf("5. relais\n");
+}
+
+
+
+void enregistrerPerformance(const char *nom_fichier, Performance performance) {
+    FILE *fichier = fopen(nom_fichier, "a"); // Mode append pour ajouter sans écraser
     if (fichier == NULL) {
         printf("Erreur lors de l'ouverture du fichier %s\n", nom_fichier);
         return;
@@ -36,12 +57,12 @@ void enregistrerAthlete(const char *nom_fichier, Performance performance) {
     printf("Performance enregistrée pour %s\n", nom_fichier);
 }
 
-void saisirPerformancesAthlete(Athlete *athlete) {
+void saisirPerformancesAthlete(Athlete *athlete, const char *nom_fichier) {
     printf("Nombre de performances à entrer : ");
     scanf("%d", &athlete->nbPerformances);
 
     for (int i = 0; i < athlete->nbPerformances; i++) {
-        printf("Performance %d:\n", i+1);
+        printf("Performance %d:\n", i + 1);
         printf("Date (AAAA-MM-JJ) : ");
         scanf("%s", athlete->performances[i].date);
         printf("Type d'épreuve : ");
@@ -50,14 +71,11 @@ void saisirPerformancesAthlete(Athlete *athlete) {
         scanf("%f", &athlete->performances[i].temps);
         athlete->performances[i].position_relais = -1;  // Modifier si relais
 
-        char filename[64];
-        snprintf(filename, sizeof(filename), "%s_performances.txt", athlete->nom);
-        enregistrerAthlete(filename, athlete->performances[i]);
+        enregistrerPerformance(nom_fichier, athlete->performances[i]);
     }
 }
 
-void chargerPerformances(const char *nom_fichier, Performance*performances, int *nbPerformances) {
-    // Ouvrir le fichier en mode lecture
+void chargerPerformances(const char *nom_fichier, Performance* performances, int *nbPerformances) {
     FILE *fichier = fopen(nom_fichier, "r");
     if (!fichier) {
         printf("Impossible d'ouvrir le fichier %s pour lecture.\n", nom_fichier);
@@ -65,10 +83,7 @@ void chargerPerformances(const char *nom_fichier, Performance*performances, int 
         return;
     }
 
-    // Initialiser le compteur de performances
     *nbPerformances = 0;
-
-    // Lire le fichier et stocker les performances dans le tableau
     while (fscanf(fichier, "%s %s %f %d", 
                   performances[*nbPerformances].date, 
                   performances[*nbPerformances].epreuve, 
@@ -76,54 +91,16 @@ void chargerPerformances(const char *nom_fichier, Performance*performances, int 
                   &performances[*nbPerformances].position_relais) == 4) {
         (*nbPerformances)++;
     }
-
-    // Fermer le fichier après la lecture
     fclose(fichier);
 }
 
-void updatePerformance(const char *filename, Performance performance) {
-    FILE *file = fopen(filename, "ab+"); // Ouverture du fichier en mode lecture/écriture binaire, création s'il n'existe pas
-
-    if (file == NULL) {
-        printf("Erreur : Impossible d'ouvrir le fichier %s.\n", filename);
-        return;
-    }
-
-    // Recherche de la performance dans le fichier
-    int found = 0;
-    rewind(file); // Retour au début du fichier
-    while (fread(&performance, sizeof(Performance), 1, file)) {
-        // Comparaison des dates et des types d'épreuve
-        if (strcmp(performance.date, performance.date) == 0 && strcmp(performance.epreuve, performance.epreuve) == 0) {
-            // Performance trouvée, mise à jour
-            fseek(file, -sizeof(Performance), SEEK_CUR); // Retour à la position avant la lecture
-            fwrite(&performance, sizeof(Performance), 1, file); // Écriture de la nouvelle performance
-            found = 1;
-            break;
-        }
-    }
-
-    // Si la performance n'a pas été trouvée, elle est ajoutée à la fin du fichier
-    if (!found) {
-        fwrite(&performance, sizeof(Performance), 1, file);
-    }
-
-    fclose(file); // Fermeture du fichier
-}
-
 void afficherHistoriqueAthlete(const char *nom_fichier) {
-    // Construire le nom complet du fichier basé sur le nom de l'athlète
-    char nomCompletFichier[64];
-    snprintf(nomCompletFichier, sizeof(nomCompletFichier), "%s.txt", nom_fichier);
-
-    // Ouvrir le fichier en mode lecture
-    FILE *fichier = fopen(nomCompletFichier, "r");
+    FILE *fichier = fopen(nom_fichier, "r");
     if (!fichier) {
-        printf("Impossible d'ouvrir le fichier %s pour lecture.\n", nomCompletFichier);
+        printf("Impossible d'ouvrir le fichier %s pour lecture.\n", nom_fichier);
         return;
     }
 
-    // Lire et afficher les performances de l'athlète
     Performance performance;
     printf("Historique des performances pour %s :\n", nom_fichier);
     while (fscanf(fichier, "%s %s %f %d", 
@@ -136,19 +113,13 @@ void afficherHistoriqueAthlete(const char *nom_fichier) {
         }
         printf("\n");
     }
-
-    // Fermer le fichier
     fclose(fichier);
 }
 
-
 void afficherStatistiques(const char *nom_fichier) {
-    char nomCompletFichier[64];
-    snprintf(nomCompletFichier, sizeof(nomCompletFichier), "%s.txt", nom_fichier);
-    
-    FILE *fichier = fopen(nomCompletFichier, "r");
+    FILE *fichier = fopen(nom_fichier, "r");
     if (!fichier) {
-        printf("Impossible d'ouvrir le fichier %s pour lecture.\n", nomCompletFichier);
+        printf("Impossible d'ouvrir le fichier %s pour lecture.\n", nom_fichier);
         return;
     }
 
@@ -161,11 +132,9 @@ void afficherStatistiques(const char *nom_fichier) {
     }
     fclose(fichier);
 
-    // Tableau pour stocker les statistiques des différentes épreuves
     StatistiquesEpreuve statistiques[20]; // Supposons qu'il n'y a pas plus de 20 épreuves différentes
     int nombreEpreuves = 0;
 
-    // Initialiser les statistiques pour chaque épreuve
     for (int i = 0; i < count; i++) {
         int found = -1;
         for (int j = 0; j < nombreEpreuves; j++) {
@@ -181,17 +150,9 @@ void afficherStatistiques(const char *nom_fichier) {
             statistiques[found].pireTemps = FLT_MIN;
             statistiques[found].tempsMoyen = 0;
         }
-        // Mettre à jour les statistiques pour cette épreuve
-        if (performances[i].temps < statistiques[found].meilleurTemps) {
-            statistiques[found].meilleurTemps = performances[i].temps;
-        }
-        if (performances[i].temps > statistiques[found].pireTemps) {
-            statistiques[found].pireTemps = performances[i].temps;
-        }
-        statistiques[found].tempsMoyen += performances[i].temps;
+        
     }
 
-    // Calculer la moyenne et afficher les résultats
     for (int i = 0; i < nombreEpreuves; i++) {
         statistiques[i].tempsMoyen /= count; // Moyenne sur le nombre total de performances, pas par épreuve
         printf("Épreuve : %s\n", statistiques[i].epreuve);
@@ -201,114 +162,247 @@ void afficherStatistiques(const char *nom_fichier) {
     }
 }
 
-int partitionParDate(Performance *performances, int gauche, int droite) {
-    char *pivot = performances[droite].date;
-    int i = (gauche - 1);
-    for (int j = gauche; j <= droite - 1; j++) {
-        if (strcmp(performances[j].date, pivot) < 0) {
-            i++;
-            Performance temp = performances[i];
-            performances[i] = performances[j];
-            performances[j] = temp;
+FILE *ouvrirAthlete(int choix){
+    int choixAthlete;
+    char nomFicher[100];
+    char nomAthlete[100];
+
+    FILE *Athlete = fopen("/workspaces/PROJET-JO-/Athlete.txt", "r");
+
+    while(fgets(nomAthlete, sizeof(Athlete), Athlete)){
+        sscanf(nomAthlete, "%d", &choixAthlete);
+        nomAthlete[strcspn(nomAthlete, "\n")] = 0;
+
+        if(choixAthlete == choix){
+            printf("%s : \n", nomAthlete + 2);
+            break;
         }
     }
-    Performance temp = performances[i + 1];
-    performances[i + 1] = performances[droite];
-    performances[droite] = temp;
-    return (i + 1);
+
+    sprintf(nomFicher, "/workspaces/PROJET-JO-/%s.txt", nomAthlete + 2);
+
+    FILE *file = fopen(nomFicher, "r");
+
+    return file;
 }
 
-void quickSortParDate(Performance *performances, int gauche, int droite) {
-    if (gauche < droite) {
-        int pivotIndex = partitionParDate(performances, gauche, droite);
-        quickSortParDate(performances, gauche, pivotIndex - 1);
-        quickSortParDate(performances, pivotIndex + 1, droite);
-    }
-}
+int ligne(FILE *file){
+    int c;
+    int lignes = 0;
 
-void trierPerformancesParDate(Performance *performances, int nbPerformances) {
-    quickSortParDate(performances, 0, nbPerformances - 1);
-}
-
-int partitionParEpreuve(Performance *performances, int gauche, int droite) {
-    char *pivot = performances[droite].epreuve;
-    int i = (gauche - 1);
-    for (int j = gauche; j <= droite - 1; j++) {
-        if (strcmp(performances[j].epreuve, pivot) < 0) {
-            i++;
-            Performance temp = performances[i];
-            performances[i] = performances[j];
-            performances[j] = temp;
+    while((c = fgetc(file)) != EOF){
+        if (c == '\n'){
+            lignes++;
         }
     }
-    Performance temp = performances[i + 1];
-    performances[i + 1] = performances[droite];
-    performances[droite] = temp;
-    return (i + 1);
+    return lignes;
 }
 
-void quickSortParEpreuve(Performance *performances, int gauche, int droite) {
-    if (gauche < droite) {
-        int pivotIndex = partitionParEpreuve(performances, gauche, droite);
-        quickSortParEpreuve(performances, gauche, pivotIndex - 1);
-        quickSortParEpreuve(performances, pivotIndex + 1, droite);
+void afficherNomEpreuve(FILE *file){
+    char nomEpreuve[100];
+
+    fseek(file, 2, SEEK_CUR);
+    fgets(nomEpreuve, sizeof(nomEpreuve), file);
+    nomEpreuve[strcspn(nomEpreuve, "\n")] = 0;
+    printf("%s\n", nomEpreuve);
+}
+
+void ListeEpreuve(FILE *file){
+    int lignes = ligne(file);
+    rewind(file);
+    printf("\n");
+
+    for(int i=0; i<lignes; i++){
+        printf("%d. ", i+1);
+        afficherNomEpreuve(file);
     }
 }
 
+void afficherNomAthlete(FILE *file){
+    char nomAthlete[100];
 
-void trierPerformancesParEpreuve(Performance *performances, int nbPerformances) {
-    quickSortParEpreuve(performances, 0, nbPerformances - 1);
+    fseek(file, 2, SEEK_CUR);
+    fgets(nomAthlete, sizeof(nomAthlete), file);
+    nomAthlete[strcspn(nomAthlete, "\n")] = 0;
+    printf("%s\n", nomAthlete);
 }
 
+void ListeAthlete(FILE *file){
+    int lignes = ligne(file);
+    rewind(file);
+    printf("\n");
 
-// Programme principal
+    for(int i=0; i<lignes; i++){
+        printf("%d. ", i+1);
+        afficherNomAthlete(file);
+    }
+}
+
+void kk(){
+    Athlete athlete;
+    Performance performance;
+    printf("Nom de l'athlète (lucie, sarah, jack) : ");
+                scanf("%s", athlete.nom);
+
+                printf("Date (jour, mois, année) : ");
+                scanf("%d %d %d", &performance.date.jour, &performance.date.mois, &performance.date.annee);
+
+                typeEpreuves();
+                printf("Type d'épreuve : ");
+                scanf("%d", &performance.epreuve);
+
+                printf("Temps réalisé : \n");
+                printf("Minute :");
+                scanf("%d", &performance.temps.minute);
+                printf("Seconde :");
+                scanf("%d", &performance.temps.seconde);
+                printf("Ms :");
+                scanf("%d", &performance.temps.ms);
+
+                printf("\n");
+
+                printf("L'athlète %s a effectué un temps de %dmin %dsec %dms\n", athlete.nom, performance.temps.minute, performance.temps.seconde, performance.temps.ms);
+}
+
+void afficherAthlete(Performance performance){
+    char nomAthlete[100];
+    int choix;
+
+    FILE *file = fopen("/workspaces/PROJET-JO-/Athlete.txt", "r");
+    if(file == NULL){
+        printf("Erreur ouverture fichier\n");
+        return;
+    }
+
+    ListeAthlete(file);
+    
+    printf("Choix : ");
+    scanf("%d", &choix);
+
+    printf("\n");
+    FILE *Athlete = ouvrirAthlete(choix);
+    if(Athlete == NULL){
+        printf("Erreur ouverture fichier\n");
+    }
+
+    //Sauter une ligne dans le fichier de l'athlete
+    while(fgetc(Athlete) != '\n');
+
+    while(fscanf(Athlete, "%d %d %d %s %d %d %d %d", &performance.date.jour, &performance.date.mois, &performance.date.annee, performance.epreuve, &performance.temps.minute, &performance.temps.seconde, &performance.temps.ms, &performance.position_relais) != EOF){
+        printf("Le %d/%d/%d\n", performance.date.jour, performance.date.mois, performance.date.annee);
+        printf("Temps : %dmin %dsec %dms\n", performance.temps.minute, performance.temps.seconde, performance.temps.ms);
+        if(strcmp(performance.epreuve, "relais") == 0){
+            printf("Position relais : %d\n", performance.position_relais);
+        }
+        printf("\n");
+    }
+
+    fclose(Athlete);
+}
+
+void creeAthlete(){
+    char prenom[100], chemin[100], ligne[100];
+    int dernierNumero = 0;
+
+    FILE *nomAthletes = fopen("/workspaces/PROJET-JO-/Athlete.txt", "r");
+    if(nomAthletes == NULL){
+        printf("Erreur ouverture fichier");
+    }else{
+        while(fgets(ligne, sizeof(ligne), nomAthletes) != NULL){
+            sscanf(ligne, "%d", &dernierNumero);
+        }
+        fclose(nomAthletes);
+    }
+
+    printf("Quel est le prénom : ");
+    scanf("%s", prenom);
+
+    sprintf(chemin, "/workspaces/PROJET-JO-/%s.txt", prenom);
+
+    FILE *file = fopen(chemin, "w");
+    if(file == NULL){
+        printf("Erreur ouverture fichier");
+        return;
+    }
+    fprintf(file, "%s\n", prenom);
+    fclose(file);
+
+    nomAthletes = fopen("/workspaces/PROJET-JO-/Athlete.txt", "a");
+    if(nomAthletes != NULL){
+        fprintf(nomAthletes, "%d %s\n", dernierNumero+1, prenom);
+        fclose(nomAthletes);
+    }
+}
+
+void ajouterEntrainement(){
+    int choix, choixEpreuve;
+
+    FILE *nomAthletes = fopen("/workspaces/PROJET-JO-/Athlete.txt", "r");
+    if(nomAthletes == NULL){
+        printf("Erreur ouverture fichier\n");
+        return;
+    }
+
+    FILE *nomEpreuve = fopen("/workspaces/PROJET-JO-/Epreuves.txt", "r");
+    if(nomEpreuve == NULL){
+        printf("Erreur ouverture fichier");
+        return;
+    }
+
+
+    ListeAthlete(nomAthletes);
+    printf("Choix : ");
+    scanf("%d", &choix);
+
+    printf("\n");
+    FILE *Athlete = ouvrirAthlete(choix);
+    if(Athlete == NULL){
+        printf("Erreur ouverture fichier\n");
+    }
+
+    ListeEpreuve(nomEpreuve);
+    printf("Choix : ");
+    scanf("%d", &choixEpreuve);
+
+}
+
 int main() {
-    // Initialisation des variables nécessaires
     Athlete athlete;
     Performance performance;
     int choix;
-    
-    // Menu principal pour interagir avec l'utilisateur
+
     while (1) {
         printf("\nMenu Principal:\n");
         printf("1. Entrer un nouvel athlète\n");
-        printf("2. Entrer une nouvelle performance\n");
+        printf("2. Entrer une nouvelle performance pour un athlète existant\n");
         printf("3. Afficher l'historique d'un athlète\n");
         printf("4. Afficher les statistiques d'un athlète\n");
         printf("5. Quitter\n");
         printf("Votre choix : ");
         scanf("%d", &choix);
-        
+
         switch (choix) {
             case 1:
-                printf("Nom de l'athlète : ");
-                scanf("%s", athlete.nom);
-                saisirPerformancesAthlete(&athlete);
+                creeAthlete();
                 break;
             case 2:
-                printf("Nom de l'athlète : ");
-                scanf("%s", athlete.nom);
-                printf("Date (AAAA-MM-JJ) : ");
-                scanf("%s", performance.date);
-                printf("Type d'épreuve : ");
-                scanf("%s", performance.epreuve);
-                printf("Temps réalisé : ");
-                scanf("%f", &performance.temps);
-                performance.position_relais = -1;  // Modifier si relais
-                char filename[64];  // Assurez-vous que la longueur est suffisante pour contenir le nom de fichier
-                snprintf(filename, sizeof(filename), "%s_performances.txt", athlete.nom);
-                // Enregistrer la performance
-                enregistrerAthlete(filename, performance);
+                ajouterEntrainement();
                 break;
             case 3:
-                printf("Nom de l'athlète pour l'historique : ");
-                scanf("%s", athlete.nom);
-                afficherHistoriqueAthlete(athlete.nom);
+                afficherAthlete(performance);
                 break;
             case 4:
-                printf("Nom de l'athlète pour les statistiques : ");
+                printf("Nom de l'athlète pour les statistiques (lucie, sarah, jack) : ");
                 scanf("%s", athlete.nom);
-                afficherStatistiques(athlete.nom);
+                if (strcmp(athlete.nom, "lucie") == 0) {
+                    afficherStatistiques("lucie.txt");
+                } else if (strcmp(athlete.nom, "sarah") == 0) {
+                    afficherStatistiques("sarah.txt");
+                } else if (strcmp(athlete.nom, "jack") == 0) {
+                    afficherStatistiques("jack.txt");
+                } else {
+                    printf("Nom d'athlète invalide.\n");
+                }
                 break;
             case 5:
                 return 0;
@@ -316,6 +410,5 @@ int main() {
                 printf("Choix invalide, veuillez réessayer.\n");
         }
     }
-    
     return 0;
 }
